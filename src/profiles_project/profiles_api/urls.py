8dbf36d1c109e9e0a7bb33636b0b3987d9f28bda
John <johnmcguin88@gmail.com>
2018-11-17 12:50:27 -0700

    added first viewset and router

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url
+from django.conf.urls import include
+
+# used to route to API View SEts
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
+    # no url because it's the default router
+    url(r'', include(router.urls))
 ]
